02f73340edd90134750f62a8b86bbb74dc5ab9df
ShotaKawatsuji <k021g1403@g.neec.ac.jp>
2022-12-15 11:37:36 +0900

    完了

--- Resources/shaders/OBJPixelShader.hlsl
+++ Resources/shaders/OBJPixelShader.hlsl
@@ -7,6 +7,25 @@ float4 main(VSOutput input) : SV_TARGET
 {
 	// テクスチャマッピング
 	float4 texcolor = tex.Sample(smp,input.uv);
+	// シェーディングによる色
+	float4 shadecolor;
+	// 光沢度
+	const float shininess = 4.0f;
+	// 頂点から視点への方向ベクトル
+	float3 eyedir = normalize(cameraPos - input.worldpos.xyz);
+	// ライトに向かうベクトルと法線の内積
+	float3 dotlightnormal = dot(lightv, input.normal);
+	// 反射光ベクトル
+	float3 reflect = normalize(-lightv + 2 * dotlightnormal * input.normal);
+	// 環境反射光
+	float3 ambient = m_ambient;
+	// 拡散反射光
+	float3 diffuse = dotlightnormal * m_diffuse;
+	// 鏡面反射光
+	float3 specular = pow(saturate(dot(reflect, eyedir)), shininess) * m_specular;
+	// 全て加算する
+	shadecolor.rgb = (ambient + diffuse + specular) * lightcolor;
+	shadecolor.a = m_alpha;
 	// テクスチャの色は使わず色を数値設定
-	return input.color * texcolor;
+	return shadecolor * texcolor;
 }
--- Resources/shaders/OBJVertexShader.hlsl
+++ Resources/shaders/OBJVertexShader.hlsl
@@ -3,37 +3,15 @@
 VSOutput main(float4 pos : POSITION, float3 normal : NORMAL,
 	float2 uv : TEXCOORD)
 {
-	//// 右、下、奥の方向を向いたライト
-	//float3 lightdir = float3(1, -1, 1);
-	//lightdir = normalize(lightdir);
-	//// ライトの色(白)
-	//float3 lightcolor = float3(1, 1, 1);
-
 	// 法線にワールド行列によるスケーリング・回転を適用
 	float4 wnormal = normalize(mul(world, float4(normal, 0)));
 	float4 wpos = mul(world, pos);
 	// ピクセルシェーダーに渡す値
 	VSOutput output;
 	output.svpos = mul(mul(viewproj, world), pos);
-	// Lambert反射の計算
-	output.color.rgb = dot(lightv, wnormal.xyz) * m_diffuse * lightcolor;
-	output.color.a = m_alpha;
+	output.worldpos = wpos;
+	output.normal = wnormal.xyz;
 	output.uv = uv;
-	// 環境反射光
-	float3 ambient = m_ambient;
-	// 拡散反射光
-	float3 diffuse = dot(lightv, wnormal.xyz) * m_diffuse;
-	// 光沢度
-	const float shininess = 4.0f;
-	// 頂点から視点への方向ベクトル
-	float3 eyedir = normalize(cameraPos - wpos.xyz);
-	// 反射光ベクトル
-	float3 reflect = normalize(-lightv + 2 * dot(lightv, wnormal.xyz) * wnormal.xyz);
-	// 鏡面反射光
-	float3 specular = pow(saturate(dot(reflect, eyedir)), shininess) * m_specular;
-	// 全て加算する
-	output.color.rgb = (ambient + diffuse + specular) * lightcolor;
-	output.color.a = m_alpha;
 
 	return output;
 }
